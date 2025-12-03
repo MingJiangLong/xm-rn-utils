0371f8b06d6e35900d10e750cce0a0660d8a39e0
绣虎 <121874694@qq.com>
2025-12-03 17:34:37 +0800

    导出

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 export { default as XmError, TimeoutError, FormFieldError, NotAuthenticatedError } from './error'
 export { default as addTimeout } from './add-timeout'
-export { default as toFixed } from './add-timeout'
+export { default as toFixed } from './to-fixed'
